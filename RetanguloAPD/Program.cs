76be794431d084e8e795a5fc502f77fb2f5f4a98
elayneolima <elayne04lima@gmail.com>
2025-01-07 03:24:43 -0300

    adicionando resolucao de projeto pratico

---
+++ RetanguloAPD/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+using RetanguloAPD;
+
+Retangulo retangulo = new Retangulo();
+
+Console.WriteLine("Entre com a Largura e Altura do Retangulo: "); 
+
+retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("AREA: " + retangulo);
---
+++ RetanguloAPD/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Globalization; 
+namespace RetanguloAPD
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() 
+        {
+            return 2 * (Largura + Altura); 
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+        public override string ToString()
+        {
+            return "ÁREA: "
+                   + Area().ToString("F2", CultureInfo.InvariantCulture)
+                   + ", PERIMETRO: "
+                   + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                   + ", DIAGONAL: "
+                   + Diagonal().ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+       
+    }
+}
